f53d127388480dee8886ac4198983864a7793745
firwar <thejasonliu@gmail.com>
2013-07-13 08:55:52 +0000

    was using buf.toString wrong

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   mybuffer = new Buffer(256);
   mybuffer = fs.readFileSync('index.html');
-  var mystring = mybuffer.toString(mybuffer);
+  var mystring = mybuffer.toString();
   response.send(mystring);
 });
 
